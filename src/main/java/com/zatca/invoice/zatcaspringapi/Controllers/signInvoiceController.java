76e322c6638e8c2c94cc748338b182f16ffd2e78
LouzirMohamedAziz <medaziz.louzir@gmail.com>
2023-06-04 16:45:53 +0100

    signInvoice method creation

--- src/main/java/com/zatca/invoice/zatcaspringapi/Controllers/signInvoiceController.java
+++ src/main/java/com/zatca/invoice/zatcaspringapi/Controllers/signInvoiceController.java
@@ -1,30 +1,52 @@
 package com.zatca.invoice.zatcaspringapi.Controllers;
 
 
+import org.apache.logging.log4j.core.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 
-import com.zatca.sdk.dto.ApplicationPropertyDto;
-import com.zatca.sdk.service.InvoiceSigningService;
+import com.zatca.invoice.zatcaspringapi.Models.InvoiceRequest;
+import com.zatca.invoice.zatcaspringapi.Services.InvoiceSignService;
 
-@RequestMapping("/invoice/")
+@RequestMapping("/invoice")
 @RestController
 public class signInvoiceController {
 
-    ApplicationPropertyDto applicationPropertyDto;
-    InvoiceSigningService invoiceSigningService;
-    
-    @PostMapping("/sign")
-    public boolean signInvoice() {
-        return invoiceSigningService.signInvoice();
+    private final InvoiceSignService invoiceSignService;
+    Logger log;
+
+    @Autowired
+    public signInvoiceController(InvoiceSignService invoiceSignService) {
+        this.invoiceSignService = invoiceSignService;
     }
 
 
-    @PostMapping("/generate")
-    public boolean generateSignedInvoice(ApplicationPropertyDto apd){
-        return invoiceSigningService.generateOutput();
+    @PostMapping()
+    public String signInvoice(@RequestBody InvoiceRequest invoiceRequest){
+        try {
+            invoiceSignService.generateSignedInvoice(invoiceRequest);
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            HttpStatus statusCode = e.getStatusCode();
+            String responseBody = e.getResponseBodyAsString();
+            if (statusCode.is4xxClientError()) {
+                log.error("Client error occurred. Status Code: {}, Response: {}", statusCode, responseBody);
+            } else if (statusCode.is5xxServerError()) {
+                log.error("Server error occurred. Status Code: {}, Response: {}", statusCode, responseBody);
+            } else {
+                log.error("HTTP error occurred. Status Code: {}, Response: {}", statusCode, responseBody);
+            }
+        } catch (Exception e) {
+            log.error("An unexpected error occurred during signInvoice", e);
+        }
+        return "Invoice signed successfully!";
     }
+
 
 
 }

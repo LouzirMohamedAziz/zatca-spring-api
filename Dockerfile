30a7460657a30ed56953c1b40d547b1e33b88c18
LouzirMohamedAziz <medaziz.louzir@gmail.com>
2023-06-24 14:27:49 +0100

    using docker-compose for jenkins and nexus

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,6 @@
-# FROM openjdk:11
-# VOLUME /tmp
-# EXPOSE 8080
-# ADD lib /app/lib
-# ARG JAR_FILE=target/zatca-spring-api-0.0.1-SNAPSHOT.jar
-# COPY ${JAR_FILE} zatca-spring-api.jar
-# ENTRYPOINT ["java", "-cp", "/app/lib/zatca-einvoicing-sdk-234-R3.2.0.jar:/app/zatca-spring-api.jar", "com.zatca.sdk.MainApp"]
-
-
-
 FROM openjdk:11
 VOLUME /tmp
 EXPOSE 8080
-ADD lib /app/lib
 ARG JAR_FILE=target/zatca-spring-api-0.0.1-SNAPSHOT.jar
 COPY ${JAR_FILE} zatca-spring-api.jar
-COPY lib/zatca-einvoicing-sdk-234-R3.2.0/Apps/zatca-einvoicing-sdk-234-R3.2.0.jar /app/lib/zatca-einvoicing-sdk-234-R3.2.0/Apps/zatca-einvoicing-sdk-234-R3.2.0.jar
-ENTRYPOINT ["java", "-cp", "/app/lib/zatca-einvoicing-sdk-234-R3.2.0/Apps/zatca-einvoicing-sdk-234-R3.2.0.jar:/app/zatca-spring-api.jar", "com.zatca.sdk.MainApp"]
-
+ENTRYPOINT ["java","-jar","/zatca-spring-api.jar"]
--- src/main/java/com/zatca/invoice/zatcaspringapi/ZatcaSpringApiApplication.java
+++ src/main/java/com/zatca/invoice/zatcaspringapi/ZatcaSpringApiApplication.java
@@ -1,12 +1,6 @@
 package com.zatca.invoice.zatcaspringapi;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
-import org.apache.logging.log4j.core.Logger;
-import org.springframework.boot.CommandLineRunner;
+// import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -16,35 +10,35 @@ import org.springframework.context.annotation.ComponentScan;
 * **/
 @ComponentScan()
 @SpringBootApplication
-public class ZatcaSpringApiApplication implements CommandLineRunner{
+public class ZatcaSpringApiApplication{
 
-	private static Logger log;
+	// private static Logger log;
 	public static void main(String[] args) {
 		SpringApplication.run(ZatcaSpringApiApplication.class, args);
         }
 
-@Override
-public void run(String... args) throws Exception {
-	String line;
-	try {
-		Runtime rt = Runtime.getRuntime();
-		String command = "bash -c 'cd /Users/louzir/Pictures/zatca-spring-api/lib/zatca-einvoicing-sdk-234-R3.2.0/Data/Input && fatoora -csr -csrConfig csr-config-example-EN.properties'";
-		Process pr = rt.exec(command);
-		InputStream inputStream = pr.getInputStream();
-		BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-		while ((line = reader.readLine()) != null) {
-			System.out.println(line);
-			log.info("The following command has been executed: "+command);
-		}
-		try {
-			int exitCode = pr.waitFor();
-			System.out.println("Exit Code:" + exitCode);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	} catch (IOException e) {
-		e.printStackTrace();
-	}
-}
+// @Override
+// public void run(String... args) throws Exception {
+// 	String line;
+// 	try {
+// 		Runtime rt = Runtime.getRuntime();
+// 		String command = "bash -c 'cd /Users/louzir/Pictures/zatca-spring-api/lib/zatca-einvoicing-sdk-234-R3.2.0/Data/Input && fatoora -csr -csrConfig csr-config-example-EN.properties'";
+// 		Process pr = rt.exec(command);
+// 		InputStream inputStream = pr.getInputStream();
+// 		BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+// 		while ((line = reader.readLine()) != null) {
+// 			System.out.println(line);
+// 			log.info("The following command has been executed: "+command);
+// 		}
+// 		try {
+// 			int exitCode = pr.waitFor();
+// 			System.out.println("Exit Code:" + exitCode);
+// 		} catch (InterruptedException e) {
+// 			e.printStackTrace();
+// 		}
+// 	} catch (IOException e) {
+// 		e.printStackTrace();
+// 	}
+// }
 	}
 

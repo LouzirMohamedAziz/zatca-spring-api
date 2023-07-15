d3002e04583b09e99f8d69564a6f591862b96f47
Mohamed Aziz LOUZIR <medaziz.louzir@gmail.com>
2023-07-15 22:44:46 +0100

    Having one method insead of three

--- src/main/java/com/zatca/invoice/zatcaspringapi/Controllers/InvoiceController.java
+++ src/main/java/com/zatca/invoice/zatcaspringapi/Controllers/InvoiceController.java
@@ -1,7 +1,5 @@
 package com.zatca.invoice.zatcaspringapi.Controllers;
 
-import java.util.Map;
-
 import org.apache.logging.log4j.core.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,11 +11,7 @@ import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
 
 import com.zatca.invoice.zatcaspringapi.Models.InvoiceRequest;
-import com.zatca.invoice.zatcaspringapi.Models.Qr;
-import com.zatca.invoice.zatcaspringapi.Models.Validation;
 import com.zatca.invoice.zatcaspringapi.Services.InvoiceSignService;
-import com.zatca.invoice.zatcaspringapi.Services.QrService;
-import com.zatca.invoice.zatcaspringapi.Services.ValidationService;
 import com.zatca.sdk.service.validation.Result;
 
 
@@ -26,88 +20,23 @@ import com.zatca.sdk.service.validation.Result;
 public class InvoiceController {
 
     private final InvoiceSignService invoiceSignService;
-    private final QrService qrService;
-    private final ValidationService validationService;
     Logger log;
 
-    public InvoiceController(InvoiceSignService invoiceSignService, QrService qrService, ValidationService validationService) {
+    public InvoiceController(InvoiceSignService invoiceSignService) {
         this.invoiceSignService = invoiceSignService;
-        this.qrService = qrService;
-        this.validationService = validationService;
     }
 
-@PostMapping
-public ResponseEntity<Object> handleInvoiceRequest(@RequestBody Map<String, Object> request) {
-    if (request.containsKey("invoicePath") && request.containsKey("invoiceName") && request.containsKey("signedInvoiceName")) {
-        InvoiceRequest invoiceRequest = new InvoiceRequest();
-        invoiceRequest.setInvoicePath((String) request.get("invoicePath"));
-        invoiceRequest.setInvoiceName((String) request.get("invoiceName"));
-        invoiceRequest.setSignedInvoiceName((String) request.get("signedInvoiceName"));
-        return signInvoice(invoiceRequest);
-    } else if (request.containsKey("signedInvoiceFile")) {
-        Validation validation = new Validation();
-        validation.setSignedInvoiceFile((String) request.get("signedInvoiceFile"));
-        return validateInvoiceImpl(validation);
-    } else if (request.containsKey("signedInvoiceFilePath")) {
-        Qr qr = new Qr();
-        qr.setSignedInvoiceFilePath((String) request.get("signedInvoiceFilePath"));
-        return createQRCode(qr);
-    } else {
-        return ResponseEntity.badRequest().body("Invalid request body");
-    }
-}
-
-
-    ResponseEntity<Object> validateInvoiceImpl(Validation validation) {
+    @PostMapping
+    private ResponseEntity<Object> signInvoice(@RequestBody InvoiceRequest invoiceRequest) {
+        
         try {
-            Result result = validationService.validityCheck(validation);
-            if (result != null) {
+            Result result = invoiceSignService.generateSignedInvoice(invoiceRequest);
+            if(result!=null) {
                 return ResponseEntity.ok(result);
-            } else {
+            }
+            else {
                 return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
             }
-        } catch (HttpClientErrorException | HttpServerErrorException e) {
-            HttpStatus statusCode = e.getStatusCode();
-            String responseBody = e.getResponseBodyAsString();
-            if (statusCode.is4xxClientError()) {
-                log.error("Client error occurred. Status Code: {}, Response: {}", statusCode, responseBody);
-            } else if (statusCode.is5xxServerError()) {
-                log.error("Server error occurred. Status Code: {}, Response: {}", statusCode, responseBody);
-            } else {
-                log.error("HTTP error occurred. Status Code: {}, Response: {}", statusCode, responseBody);
-            }
-            return ResponseEntity.status(statusCode).body(responseBody);
-        } catch (Exception e) {
-            log.error("An unexpected error occurred during validation", e);
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An unexpected error occurred during validation");
-        }
-    }
-
-    private ResponseEntity<Object> createQRCode(Qr qr) {
-        try {
-            qrService.generateQRCODE(qr);
-            return ResponseEntity.ok("QR code generated successfully!");
-        } catch (HttpClientErrorException | HttpServerErrorException e) {
-            HttpStatus statusCode = e.getStatusCode();
-            String responseBody = e.getResponseBodyAsString();
-            if (statusCode.is4xxClientError()) {
-                log.error("Client error occurred. Status Code: {}, Response: {}", statusCode, responseBody);
-            } else if (statusCode.is5xxServerError()) {
-                log.error("Server error occurred. Status Code: {}, Response: {}", statusCode, responseBody);
-            } else {
-                log.error("HTTP error occurred. Status Code: {}, Response: {}", statusCode, responseBody);
-            }
-            return ResponseEntity.status(statusCode).body(responseBody);
-        } catch (Exception e) {
-            log.error("An unexpected error occurred during QR generation", e);
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An unexpected error occurred during QR generation");
-        }
-    }
-
-    private ResponseEntity<Object> signInvoice(InvoiceRequest invoiceRequest) {
-        try {
-            invoiceSignService.generateSignedInvoice(invoiceRequest);
-            return ResponseEntity.ok("Invoice signed successfully!");
         } catch (HttpClientErrorException | HttpServerErrorException e) {
             HttpStatus statusCode = e.getStatusCode();
             String responseBody = e.getResponseBodyAsString();
--- src/main/java/com/zatca/invoice/zatcaspringapi/Services/InvoiceSignService.java
+++ src/main/java/com/zatca/invoice/zatcaspringapi/Services/InvoiceSignService.java
@@ -1,15 +1,25 @@
 package com.zatca.invoice.zatcaspringapi.Services;
 
 import java.io.File;
+import java.io.IOException;
 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+
+import org.apache.xml.security.c14n.CanonicalizationException;
+import org.apache.xml.security.c14n.InvalidCanonicalizerException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.xml.sax.SAXException;
 
+import com.zatca.invoice.zatcaspringapi.Configurations.FileLoader;
 import com.zatca.invoice.zatcaspringapi.Models.InvoiceRequest;
 import com.zatca.invoice.zatcaspringapi.Repositories.InvoiceRequestRepository;
 import com.zatca.sdk.dto.ApplicationPropertyDto;
 import com.zatca.sdk.service.GeneratorTemplate;
 import com.zatca.sdk.service.InvoiceSigningService;
+import com.zatca.sdk.service.flow.ValidationProcessorImpl;
+import com.zatca.sdk.service.validation.Result;
 
 @Service
 public class InvoiceSignService {
@@ -21,15 +31,39 @@ public class InvoiceSignService {
         this.invoiceRequestRepository = invoiceRequestRepository;
     }
 
-    public InvoiceRequest generateSignedInvoice(InvoiceRequest invoiceRequest){
+    public Result generateSignedInvoice(InvoiceRequest invoiceRequest){
+
+        // Signing the Invoice
         GeneratorTemplate generateService = new InvoiceSigningService();
         ApplicationPropertyDto configuration = new ApplicationPropertyDto();
         configuration.setGenerateSignature(true);
         configuration.setInvoiceFileName(invoiceRequest.getInvoicePath().concat(String.valueOf(File.separatorChar)).concat(invoiceRequest.getInvoiceName()));
         configuration.setOutputInvoiceFileName(invoiceRequest.getInvoicePath().concat(String.valueOf(File.separatorChar)).concat(invoiceRequest.getSignedInvoiceName()));
         generateService.generate(configuration);
-        return invoiceRequestRepository.save(invoiceRequest);
-        
-    }}
+
+        // Generating QR Code
+        String baseDir = System.getProperty("user.dir");
+        String invoiceFilePath = baseDir + "/lib/zatca-einvoicing-sdk-234-R3.2.0/signedInvoice.xml";
+        configuration.setGenerateQr(true);
+        configuration.setInvoiceFileName(invoiceRequest.getInvoicePath()+invoiceRequest.getInvoiceName());
+        configuration.setOutputInvoiceFileName(invoiceFilePath);
+        generateService.generate(configuration);
+        invoiceRequestRepository.save(invoiceRequest);
+
+        // Validation the Invoice
+        FileLoader fileLoader = new FileLoader();
+        File file = fileLoader.loadFile(invoiceFilePath);
+        ValidationProcessorImpl validationProcessorImpl = new ValidationProcessorImpl();
+        try {
+            return validationProcessorImpl.run(file);
+        } catch (InvalidCanonicalizerException | CanonicalizationException | IOException | ParserConfigurationException
+                | TransformerException | SAXException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+}
 
 
